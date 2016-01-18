5514ff9de14ed00b55b0e9398dc4a332b772375a
Troy Telford <ttelford@pariahzero.net>
2016-01-17 21:21:46 -0700

    Clean up cmdline/daemon section

--- dhdynupdate.py
+++ dhdynupdate.py
@@ -2,16 +2,16 @@
 
 # Copyright (c) 2016, Troy Telford
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # 1. Redistributions of source code must retain the above copyright notice,
 #    this list of conditions and the following disclaimer.
 # 2. Redistributions in binary form must reproduce the above copyright notice,
 #    this list of conditions and the following disclaimer in the documentation
 #    and/or other materials provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -23,7 +23,7 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 # The views and conclusions contained in the software and documentation are
 # those of the authors and should not be interpreted as representing official
 # policies, either expressed or implied.
@@ -36,7 +36,7 @@ from dhdns import dhdns
 def main(argv=None):
     if argv is None:
         argv = sys.argv
-    
+
     # read configuration from file
     config = configparser.ConfigParser()
     try:
@@ -58,9 +58,7 @@ def main(argv=None):
         exit()
 
     dh_dns = dhdns(config, config_name)
-    dh_dns.get_if_addresses()
-    dh_dns.get_dh_dns_records()
-    logging.debug(dh_dns)
+    dh_dns.update_addresses()
 
 if __name__ == "__main__":
     main()
