084ea1a3018e200f9632453518229255921cf9d2
Troy Telford <ttelford@pariahzero.net>
2016-01-17 21:20:06 -0700

    Remove unneeded methods from http_access.py
    - More thorough logging & checking

--- http_access.py
+++ http_access.py
@@ -2,16 +2,16 @@
 
 # Copyright (c) 2016, Troy Telford
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # 1. Redistributions of source code must retain the above copyright notice,
 #    this list of conditions and the following disclaimer.
 # 2. Redistributions in binary form must reproduce the above copyright notice,
 #    this list of conditions and the following disclaimer in the documentation
 #    and/or other materials provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -23,7 +23,7 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 # The views and conclusions contained in the software and documentation are
 # those of the authors and should not be interpreted as representing official
 # policies, either expressed or implied.
@@ -40,16 +40,9 @@ class http_access():
         """Initialize HTTP(S) Goo"""
         self.api_url = api_url
 
-    def get_uuid(self):
-        return uuid.uuid4()
-
-    def request_put(self):
-        """HTTP(S) PUT Request"""
-        pass
-
-    def request_post(self):
-        """HTTP(S) POST Request"""
-        pass
+    def api_request(self, request_params):
+        response = self.request_get(request_params)
+        return response_json
 
     def request_get(self, query):
         """HTTP(S) GET Request"""
@@ -62,10 +55,19 @@ class http_access():
             print(message)
             logging.critical(message)
             sys.exit()
+        dreamhost_response.close()
+        logging.debug("API URL:" + self.api_url)
         logging.debug(dreamhost_response.request.headers)
         logging.debug(dreamhost_response.request.url)
-        logging.debug("DEBUG URL:" + self.api_url) 
-        dreamhost_response.close()
-        return dreamhost_response
+        response_json = dreamhost_response.json()
+        if response_json["result"] != "success":
+            logging.error("DreamHost did not complete the request: %s"
+                          % (request_params))
+        else:
+            logging.debug("Successful Request:  %s, %s"
+                          % (response_json["result"],
+                            (json.dumps(dreamhost_response.json(),
+                            sort_keys=True, indent=4))))
+        return response_json
 
 # vim: ts=4 sw=4 et
